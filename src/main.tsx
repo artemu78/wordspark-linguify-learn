3f806d7add9e756d8af5f81f498b43064b70311d
ArtemReva <artemreva79@gmail.com>
2025-07-15 14:35:04 +0300

    Fix version display

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import pkg from "../package.json";
 
 // Log app version from package.json
-console.log('App Version: 0.0.0');
+console.log("App Version: " + pkg.version);
 
 createRoot(document.getElementById("root")!).render(<App />);
